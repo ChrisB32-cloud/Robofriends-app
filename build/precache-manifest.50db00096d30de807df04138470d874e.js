de0f3db21c8b21de5bb7ff0abcc4911fb0e9162a
ChrisBrownTheGuy <ChrisBrownTheGuy@users.noreply.github.com>
2020-02-14 10:50:15 -0600

    adding files

---
+++ build/precache-manifest.50db00096d30de807df04138470d874e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5da14f3e5e738dfd73cbe11d372a073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d22709b1ac8d5b94dccd",
+    "url": "/static/css/main.33d2178c.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22709b1ac8d5b94dccd",
+    "url": "/static/js/main.045db765.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "1c7334345a70477ef055696837ff0cd4",
+    "url": "/static/media/SEGA.1c733434.woff"
+  }
+]);
